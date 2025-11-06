e8b20822e4db4b44eecee840fe314baefcdf703e
Augustine Leonard <79272623+august1n3@users.noreply.github.com>
2025-11-07 00:53:03 +0300

    removing unnecessary components

--- pallets/member-registry/src/lib.rs
+++ pallets/member-registry/src/lib.rs
@@ -5,55 +5,20 @@
 //! as a starting point for creating a new pallet and **not meant to be used in production**.
 //!
 //! ## Overview
-//!
-//! This template pallet contains basic examples of:
-//! - declaring a storage item that stores a single block-number
-//! - declaring and using events
-//! - declaring and using errors
-//! - a dispatchable function that allows a user to set a new value to storage and emits an event
-//!   upon success
-//! - another dispatchable function that causes a custom error to be thrown
-//!
-//! Each pallet section is annotated with an attribute using the `#[pallet::...]` procedural macro.
-//! This macro generates the necessary code for a pallet to be aggregated into a FRAME runtime.
-//!
-//! To get started with pallet development, consider using this tutorial:
-//!
-//! <https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/guides/your_first_pallet/index.html>
-//!
-//! And reading the main documentation of the `frame` crate:
-//!
-//! <https://paritytech.github.io/polkadot-sdk/master/polkadot_sdk_docs/polkadot_sdk/frame_runtime/index.html>
-//!
-//! And looking at the frame [`kitchen-sink`](https://paritytech.github.io/polkadot-sdk/master/pallet_example_kitchensink/index.html)
-//! pallet, a showcase of all pallet macros.
-//!
-//! ### Pallet Sections
-//!
-//! The pallet sections in this template are:
-//!
-//! - A **configuration trait** that defines the types and parameters which the pallet depends on
-//!   (denoted by the `#[pallet::config]` attribute). See: [`Config`].
-//! - A **means to store pallet-specific data** (denoted by the `#[pallet::storage]` attribute).
-//!   See: [`storage_types`].
-//! - A **declaration of the events** this pallet emits (denoted by the `#[pallet::event]`
-//!   attribute). See: [`Event`].
-//! - A **declaration of the errors** that this pallet can throw (denoted by the `#[pallet::error]`
-//!   attribute). See: [`Error`].
-//! - A **set of dispatchable functions** that define the pallet's functionality (denoted by the
-//!   `#[pallet::call]` attribute). See: [`dispatchables`].
-//!
-//! Run `cargo doc --package pallet-member_registry --open` to view this pallet's documentation.
+
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub use pallet::*;
 
-use frame::{
+use frame_support::{
     prelude::*,
-    traits::{CheckedAdd, One},
+    traits::{EnsureOrigin, StoredMap, UnixTime},
+    BoundedVec,
 };
 
+use frame_system::prelude::*;
+use sp
 #[cfg(test)]
 mod mock;
 
@@ -71,7 +36,7 @@ mod benchmarking;
 // To see a full list of `pallet` macros and their use cases, see:
 // <https://paritytech.github.io/polkadot-sdk/master/pallet_example_kitchensink/index.html>
 // <https://paritytech.github.io/polkadot-sdk/master/frame_support/pallet_macros/index.html>
-#[frame::pallet]
+#[frame_support::pallet]
 pub mod pallet {
     use super::*;
 
