d4e4ebec561219bf9ebe215617c25e32e241b94b
Augustine Leonard <79272623+august1n3@users.noreply.github.com>
2025-11-07 11:18:58 +0300

    created mock runtimes for each pallet

--- pallets/proposal-router/src/mock.rs
+++ pallets/proposal-router/src/mock.rs
@@ -1,50 +1,124 @@
-use crate::frame_system::{mocking::MockBlock, GenesisConfig};
-use frame::{
-    deps::frame_support::{derive_impl, runtime, weights::constants::RocksDbWeight},
-    runtime::prelude::*,
-    testing_prelude::*,
-};
+//! Mock runtime for pallet-proposal-router tests.
 
-// Configure a mock runtime to test the pallet.
-#[runtime]
-mod test_runtime {
-    #[runtime::runtime]
-    #[runtime::derive(
-        RuntimeCall,
-        RuntimeEvent,
-        RuntimeError,
-        RuntimeOrigin,
-        RuntimeFreezeReason,
-        RuntimeHoldReason,
-        RuntimeSlashReason,
-        RuntimeLockId,
-        RuntimeTask
-    )]
-    pub struct Test;
+#![cfg(test)]
 
-    #[runtime::pallet_index(0)]
-    pub type System = frame_system;
-    #[runtime::pallet_index(1)]
-    pub type Proposal_router = crate;
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::{BlakeTwo256, IdentityLookup}};
+use frame_support::{parameter_types, construct_runtime};
+use frame_support::traits::Everything;
+
+use crate as pallet_proposal_router;
+use pallet_member_registry as member_registry;
+
+pub type AccountId = u64;
+pub type Balance = u128;
+pub type BlockNumber = u64;
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const SS58Prefix: u8 = 42;
+    pub const ExistentialDeposit: Balance = 1;
+    pub const DefaultVotingPeriod: u32 = 10;
+    pub const DefaultQuorum: u32 = 1;
+    pub const DefaultPassThreshold: u32 = 5000u32;
+    pub const MaxMetadataLen: u32 = 256;
+    pub const MaxVotersPerProposal: u32 = 1024;
+    pub const MinimumPeriod: u64 = 1;
 }
 
-#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+construct_runtime!(
+    pub enum Test where
+        Block = frame_system::mocking::MockBlock<Test>,
+        NodeBlock = frame_system::mocking::MockBlock<Test>,
+        UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>
+    {
+        System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+        Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+        MemberRegistry: member_registry::{Pallet, Call, Storage, Event<T>},
+        ProposalRouter: pallet_proposal_router::{Pallet, Call, Storage, Event<T>},
+    }
+);
+
+// system
 impl frame_system::Config for Test {
-    type Nonce = u64;
-    type Block = MockBlock<Test>;
-    type BlockHashCount = ConstU64<250>;
-    type DbWeight = RocksDbWeight;
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Call = Call;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<AccountId>;
+    type Header = Header;
+    type RuntimeEvent = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = SS58Prefix;
 }
 
-impl crate::Config for Test {
-    type RuntimeEvent = RuntimeEvent;
+// timestamp
+impl pallet_timestamp::Config for Test {
+    type Moment = u64;
+    type OnTimestampSet = ();
+    type MinimumPeriod = MinimumPeriod;
     type WeightInfo = ();
 }
 
-// Build genesis storage according to the mock runtime.
+// balances
+impl pallet_balances::Config for Test {
+    type RuntimeEvent = Event;
+    type Balance = Balance;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type MaxLocks = ();
+    type WeightInfo = ();
+}
+
+// member-registry wiring (used for membership checks in proposals)
+impl member_registry::Config for Test {
+    type RuntimeEvent = Event;
+    type RootClubAdminOrigin = frame_system::EnsureRoot<AccountId>;
+    type Time = Timestamp;
+    type WeightInfo = ();
+}
+
+// proposal-router config
+impl pallet_proposal_router::Config for Test {
+    type RuntimeEvent = Event;
+    type RuntimeCall = Call;
+    type MaxMetadataLen = frame_support::traits::ConstU32<MaxMetadataLen>;
+    type MaxVotersPerProposal = frame_support::traits::ConstU32<MaxVotersPerProposal>;
+    type DefaultVotingPeriod = frame_support::traits::ConstU32<DefaultVotingPeriod>;
+    type DefaultQuorum = frame_support::traits::ConstU32<DefaultQuorum>;
+    type DefaultPassThreshold = frame_support::traits::ConstU32<DefaultPassThreshold>;
+    type RouterAdminOrigin = frame_system::EnsureRoot<AccountId>;
+    type TimeProvider = Timestamp;
+    type WeightInfo = ();
+}
+
+// helper: build test ext
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    GenesisConfig::<Test>::default()
-        .build_storage()
-        .unwrap()
-        .into()
+    let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+
+    // seed balances, include an account for pallet-executor if needed (99)
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![(1u64, 1_000_000u128), (2u64, 1_000_000u128), (99u64, 1_000_000u128)],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(t);
+    ext.execute_with(|| System::set_block_number(1));
+    ext
 }
--- pallets/tcc/src/mock.rs
+++ pallets/tcc/src/mock.rs
@@ -1,50 +1,128 @@
-use crate::frame_system::{mocking::MockBlock, GenesisConfig};
-use frame::{
-    deps::frame_support::{derive_impl, runtime, weights::constants::RocksDbWeight},
-    runtime::prelude::*,
-    testing_prelude::*,
-};
+//! Mock runtime for pallet-tcc tests (wraps pallet-assets).
 
-// Configure a mock runtime to test the pallet.
-#[runtime]
-mod test_runtime {
-    #[runtime::runtime]
-    #[runtime::derive(
-        RuntimeCall,
-        RuntimeEvent,
-        RuntimeError,
-        RuntimeOrigin,
-        RuntimeFreezeReason,
-        RuntimeHoldReason,
-        RuntimeSlashReason,
-        RuntimeLockId,
-        RuntimeTask
-    )]
-    pub struct Test;
+#![cfg(test)]
 
-    #[runtime::pallet_index(0)]
-    pub type System = frame_system;
-    #[runtime::pallet_index(1)]
-    pub type Tcc = crate;
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::{BlakeTwo256, IdentityLookup}};
+use frame_support::{parameter_types, construct_runtime};
+use frame_support::traits::Everything;
+
+use crate as pallet_tcc;
+use pallet_assets as assets;
+
+pub type AccountId = u64;
+pub type Balance = u128;
+pub type BlockNumber = u64;
+pub type AssetId = u32;
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const SS58Prefix: u8 = 42;
+    pub const ExistentialDeposit: Balance = 1;
+    pub const AssetDeposit: u128 = 0;
+    pub const AssetAccountDeposit: u128 = 0;
+    pub const MetadataDepositBase: u128 = 0;
+    pub const MetadataDepositPerByte: u128 = 0;
+    pub const ApprovalDeposit: u128 = 0;
+    pub const StringLimit: u32 = 50;
 }
 
-#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+construct_runtime!(
+    pub enum Test where
+        Block = frame_system::mocking::MockBlock<Test>,
+        NodeBlock = frame_system::mocking::MockBlock<Test>,
+        UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>
+    {
+        System: frame_system::{Pallet, Call, Storage, Config, Event<T>},
+        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+        Assets: assets::{Pallet, Call, Storage, Event<T>},
+        Tcc: pallet_tcc::{Pallet, Call, Storage, Event<T>},
+    }
+);
+
+// system
 impl frame_system::Config for Test {
-    type Nonce = u64;
-    type Block = MockBlock<Test>;
-    type BlockHashCount = ConstU64<250>;
-    type DbWeight = RocksDbWeight;
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Call = Call;
+    type Index = u64;
+    type BlockNumber = BlockNumber;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<AccountId>;
+    type Header = Header;
+    type RuntimeEvent = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = SS58Prefix;
 }
 
-impl crate::Config for Test {
-    type RuntimeEvent = RuntimeEvent;
+// balances
+impl pallet_balances::Config for Test {
+    type RuntimeEvent = Event;
+    type Balance = Balance;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type MaxLocks = ();
     type WeightInfo = ();
 }
 
-// Build genesis storage according to the mock runtime.
+// assets
+impl assets::Config for Test {
+    type RuntimeEvent = Event;
+    type Balance = Balance;
+    type AssetId = AssetId;
+    type Currency = Balances;
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+    type AssetDeposit = AssetDeposit;
+    type AssetAccountDeposit = AssetAccountDeposit;
+    type MetadataDepositBase = MetadataDepositBase;
+    type MetadataDepositPerByte = MetadataDepositPerByte;
+    type ApprovalDeposit = ApprovalDeposit;
+    type StringLimit = StringLimit;
+    type Freezer = ();
+    type Extra = ();
+    type WeightInfo = ();
+}
+
+// tcc config
+parameter_types! {
+    pub const TccAssetIdParam: u32 = 1000u32;
+}
+impl pallet_tcc::Config for Test {
+    type RuntimeEvent = Event;
+    type Currency = Assets;
+    type AssetId = AssetId;
+    type Balance = Balance;
+    type TccAssetId = frame_support::traits::ConstU32<{ TccAssetIdParam }>;
+    type InstantiateOrigin = frame_system::EnsureRoot<AccountId>;
+    type MintOrigin = frame_system::EnsureRoot<AccountId>;
+    type BurnOrigin = frame_system::EnsureRoot<AccountId>;
+    type WeightInfo = ();
+}
+
+// helper: build test ext
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    GenesisConfig::<Test>::default()
-        .build_storage()
-        .unwrap()
-        .into()
+    let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+
+    // seed balances (used for fees & asset-backed transfers)
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![(1u64, 1_000_000u128), (2u64, 1_000_000u128), (99u64, 1_000_000u128)],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(t);
+    ext.execute_with(|| System::set_block_number(1));
+    ext
 }
